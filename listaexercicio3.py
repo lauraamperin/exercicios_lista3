1f9af935a7d8889450cdbe8cc0acb773dbe500cc
Laura <lauraamperin@gmail.com>
2025-06-10 11:49:20 -0300

    listaexercicio3.py

--- listaexercicio3.py
+++ listaexercicio3.py
@@ -26,7 +26,7 @@ st.set_page_config(
 st.header("Lista de Exercício 3")
 
 import pandas as pd
-arquivo = "/https://raw.githubusercontent.com/lauraamperin/exercicios_lista3/refs/heads/main/projetos.csv"
+arquivo = "raw.githubusercontent.com/lauraamperin/exercicios_lista3/refs/heads/main/projetos.csv"
 df_projetos = pd.read_csv(arquivo, sep=";")
 st.dataframe(df_projetos)
 
